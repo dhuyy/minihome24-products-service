d96361127eb28c2def6492331cf153fa7078a755
Vandhuy <vandhuy@gmail.com>
2018-02-28 03:08:15 -0300

    Checking for NODE_ENV=test

--- samples/mongodb.js
+++ samples/mongodb.js
@@ -7,14 +7,16 @@
  https://www.npmjs.com/package/mongoose
 */
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+if (process.env.NODE_ENV !== 'test') {
+  var MongoClient = require('mongodb').MongoClient
+      , assert = require('assert');
 
 // Connection URL configured in your docker-compose.yml file
-var url = process.env.DATABASE_URL;
+  var url = process.env.DATABASE_URL;
 
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to MongoDB server");
-  db.close();
-});
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to MongoDB server");
+    db.close();
+  });
+}
